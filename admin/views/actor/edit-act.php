b13807c6c06ac292d46a6bd4fe7c80f5a5d268bf
FathimahN <fath20233ti@student.nurulfikri.ac.id>
2023-06-05 18:20:37 +0700

    update sidebar actors

--- admin/views/actor/edit-act.php
+++ admin/views/actor/edit-act.php
@@ -17,7 +17,7 @@
             <!-- Sidebar Start -->
             <nav id="sidebar" class="sidebar js-sidebar">
                 <div class="sidebar-content js-simplebar">
-                    <a class="sidebar-brand" href="index.php">
+                    <a class="sidebar-brand" href="../../index.php">
                         <img src="../../../assets/img/icons/logo-1.png" alt="logo" width="50%" 
                             style="text-align: center;">
                     </a>
@@ -29,37 +29,37 @@
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="user/users.php">
+                            <a class="sidebar-link" href="../user/user.php">
                                 <i class="align-middle" data-feather="users"></i> 
                                 <span class="align-middle">Users</span>
                             </a>
                         </li>
-                        <li class="sidebar-item  active">
+                        <li class="sidebar-item active">
                             <a class="sidebar-link" href="actor.php">
                                 <i class="align-middle" data-feather="users"></i> 
                                 <span class="align-middle">Actors</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="genre.php">
+                            <a class="sidebar-link" href="../genre/genre.php">
                                 <i class="align-middle" data-feather="list"></i> 
                                 <span class="align-middle">Genre</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="film.php">
+                            <a class="sidebar-link" href="../film/film.php">
                                 <i class="align-middle" data-feather="film"></i> 
                                 <span class="align-middle">Film</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="peran/peran.php">
+                            <a class="sidebar-link" href="../peran/peran.php">
                                 <i class="align-middle" data-feather="cast"></i> 
                                 <span class="align-middle">Peran</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a class="sidebar-link" href="kritik.php">
+                            <a class="sidebar-link" href="../kritik/kritik.php">
                                 <i class="align-middle" data-feather="message-square"></i> 
                                 <span class="align-middle">Kritik</span>
                             </a>
