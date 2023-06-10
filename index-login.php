b46b6c7c1e0a75642be13bc34c052871c22b1a69
rabiatulfitria <fitriarobiatul13.com>
2023-06-10 23:57:02 +0700

    update file login

--- index-login.php
+++ index-login.php
@@ -1,60 +1,39 @@
 <!DOCTYPE html>
-<!-- Website - www.codingnepalweb.com -->
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="description" content=" Today in this blog you will learn how to create a responsive Login & Registration Form in HTML CSS & JavaScript. The blog will cover everything from the basics of creating a Login & Registration in HTML, to styling it with CSS and adding with JavaScript." />
-    <meta
-      name="keywords"
-      content=" 
- Animated Login & Registration Form,Form Design,HTML and CSS,HTML CSS JavaScript,login & registration form,login & signup form,Login Form Design,registration form,Signup Form,HTML,CSS,JavaScript,
-"
-    />
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="assets/css/login-style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+  <script href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <title>Login & Signup Form HTML CSS | CodingNepal</title>
-    <link rel="stylesheet" href="assets/css/style-login.css" />
-    <script src="custom-scripts.js" defer></script> <!-- tidak tau dimana letak custom-script.js, jadi error di console -->
-  </head>
-  <body>
-    <section class="wrapper">
-      <div class="form signup">
-        <header>Signup</header>
-        <form action="#">
-          <input type="text" placeholder="Full name" required />
-          <input type="text" placeholder="Email address" required />
-          <input type="password" placeholder="Password" required />
-          <div class="checkbox">
-            <input type="checkbox" id="signupCheck" />
-            <label for="signupCheck">I accept all terms & conditions</label>
-          </div>
-          <input type="submit" value="Signup" />
-        </form>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="logo">
+      <img src="assets/img/icons/logo-1.png" alt="">
+    </div>
+    <div class="text-center mt-4 name">
+      MovApp
+    </div>
+    <form class="p-3 mt-3">
+      <div class="form-field d-flex align-items-center">
+        <!-- <span class="far fa-user"></span> -->
+        <input type="text" name="userName" id="userName" placeholder="Username">
       </div>
-
-      <div class="form login">
-        <header>Login</header>
-        <form action="#">
-          <input type="text" placeholder="Email address" required />
-          <input type="password" placeholder="Password" required />
-          <a href="#">Forgot password?</a>
-          <input type="submit" value="Login" />
-        </form>
+      <div class="form-field d-flex align-items-center">
+        <!-- <span class="fas fa-key"></span> -->
+        <input type="password" name="password" id="pwd" placeholder="Password">
       </div>
-
-      <script>
-        const wrapper = document.querySelector(".wrapper"),
-          signupHeader = document.querySelector(".signup header"),
-          loginHeader = document.querySelector(".login header");
-
-        loginHeader.addEventListener("click", () => {
-          wrapper.classList.add("active");
-        });
-        signupHeader.addEventListener("click", () => {
-          wrapper.classList.remove("active");
-        });
-      </script>
-    </section>
-  </body>
-</html>
+      <button class="btn mt-3">Login</button>
+    </form>
+    <div class="text-center fs-6">
+      <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+    </div>
+  </div>
+</body>
